459f5cc679c4f2fcf8f5b9bc708ee2686d674145
EugenioDeRosa <eugeniomaria.derosa@studio.unibo.it>
2026-05-23 14:46:27 +0200

    fix minori

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ["KAGGLE_USERNAME"] = "eugenioderosa"
 os.environ["KAGGLE_KEY"] = "KGAT_8f8c51550b799cda8ed82117f2e37ca2"
 
+
 import pandas as pd
 import numpy as np
 import torch
